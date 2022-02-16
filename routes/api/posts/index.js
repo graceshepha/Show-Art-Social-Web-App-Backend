851696d20a237310c6d08b1c22cd1a84897b47a9
xmaiying <58544027+xmaiying@users.noreply.github.com>
2022-02-16 11:32:19 -0500

    post repo

--- routes/api/posts/index.js
+++ routes/api/posts/index.js
@@ -3,91 +3,67 @@
  * @author My-Anh Chau
  *
  */
-const post = require('./post');
-const postRepository = require('../../../data/UserRepository');
 
-const router = express.Router();
-
-// routes
-
+const router = require('express').Router();
+// IL FAUT USE POSTREPOSITORY
+const userRepository = require('../../../data/PostRepository');
+// ROUTE POUR AJOUTER UN POST - CREATION D'UNE PUBLICATION
+router.post('/add', async (req, res) => {
+  try {
+    const p = req.body;
+    await userRepository.insertOne(p);
+    return res
+      .status(201)
+      .end();
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(400)
+      .json({ status: 400, message: 'Internal Server Error' });
+  }
+});
 // ROUTE POUR GET LES POSTS
 router.get('/', async (req, res) => {
   try {
-    const items = await getToutPosts(req.userId);
-    res
+    const users = await userRepository.getAll(req.userId);
+    return res
       .status(200)
-      .json(items);
-  } catch {
-    res
-      .status(500)
-      .json({ status: 500, message: 'Internal Server Error' });
-  }
-});
-
-// ROUTE POUR GET UN POST AVEC ID
-router.get('/:idPost', async (req, res) => {
-  const { idPost, idTag } = req.query;
-  try {
-    if (!idPost || !idTag) {
-      res
-        .status(400)
-        .json({ status: 400, message: 'Bad Request' });
-    } else {
-      await addPost({ idUtilisateur: req.userId, idPost, idTag });
-      res
-        .status(200)
-        .end();
-    }
+      .json(users);
   } catch (err) {
     console.error(err);
-    res
-      .status(500)
-      .json({ status: 500, message: 'Internal Server Error' });
+    return res
+      .status(400)
+      .json({ status: 400, message: 'Internal Server Error' });
   }
 });
-
-// ROUTE POUR AJOUTER UN POST
-router.post('/', async (req, res) => {
-  const { idPost, idTag } = req.query;
+// ROUTE POUR DELETE UN POST
+router.post('/del', async (req, res) => {
   try {
-    if (!idPost || !idTag) {
-      res
-        .status(400)
-        .json({ status: 400, message: 'Bad Request' });
-    } else {
-      await addPost({ idUtilisateur: req.userId, idPost, idTag });
-      res
-        .status(200)
-        .end();
-    }
+    const p = req.body;
+    await userRepository.delOne(p);
+    return res
+      .status(201)
+      .end();
   } catch (err) {
     console.error(err);
-    res
-      .status(500)
-      .json({ status: 500, message: 'Internal Server Error' });
+    return res
+      .status(400)
+      .json({ status: 400, message: 'Internal Server Error' });
   }
 });
 
-// ROUTE POUR ENLEVER UN POST
-router.delete('/p', async (req, res) => {
-  const { idPost, idTag } = req.query;
+router.post('/update', async (req, res) => {
   try {
-    if (!idPost || !idTag) {
-      res
-        .status(400)
-        .json({ status: 400, message: 'Bad Request' });
-    } else {
-      await removePost({
-        idUtilisateur: req.userId, idPost,
-      });
-      res
-        .status(200)
-        .end();
-    }
-  } catch {
-    res
-      .status(500)
-      .json({ status: 500, message: 'Internal Server Error' });
+    const p = req.body;
+    await userRepository.update(p);
+    return res
+      .status(201)
+      .end();
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(400)
+      .json({ status: 400, message: 'Internal Server Error' });
   }
 });
 
