cb542edf6651f825d2f7cf0fb3347f0ae7443eb6
xmaiying <58544027+xmaiying@users.noreply.github.com>
2022-02-16 12:35:24 -0500

    wip

--- routes/api/posts/index.js
+++ routes/api/posts/index.js
@@ -7,21 +7,10 @@ const postRepository = require('../../../data/PostRepository');
  * @author Bly Grâce Schephatia
  */
 
-<<<<<<< HEAD
-const router = require('express').Router();
-// IL FAUT USE POSTREPOSITORY
-const userRepository = require('../../../data/PostRepository');
-// ROUTE POUR AJOUTER UN POST - CREATION D'UNE PUBLICATION
-router.post('/add', async (req, res) => {
-  try {
-    const p = req.body;
-    await userRepository.insertOne(p);
-=======
 router.post('/add', async (req, res) => {
   try {
     const i = req.body;
     await postRepository.insertOne(i);
->>>>>>> 0103907c26d141ee92ec9710cac9ace4754bf9c7
     return res
       .status(201)
       .end();
@@ -32,15 +21,6 @@ router.post('/add', async (req, res) => {
       .json({ status: 400, message: 'Internal Server Error' });
   }
 });
-<<<<<<< HEAD
-// ROUTE POUR GET LES POSTS
-router.get('/', async (req, res) => {
-  try {
-    const users = await userRepository.getAll(req.userId);
-    return res
-      .status(200)
-      .json(users);
-=======
 /**
  * @description Cette route retourne tous les posts avec pagination
  */
@@ -50,7 +30,6 @@ router.get('/', async (req, res) => {
       new PaginationParameters(req).getOptions(),
     );
     return res.status(200).json(posts);
->>>>>>> 0103907c26d141ee92ec9710cac9ace4754bf9c7
   } catch (err) {
     console.error(err);
     return res
@@ -58,21 +37,6 @@ router.get('/', async (req, res) => {
       .json({ status: 400, message: 'Internal Server Error' });
   }
 });
-<<<<<<< HEAD
-// ROUTE POUR DELETE UN POST
-router.post('/del', async (req, res) => {
-  try {
-    const p = req.body;
-    await userRepository.delOne(p);
-    return res
-      .status(201)
-      .end();
-  } catch (err) {
-    console.error(err);
-    return res
-      .status(400)
-      .json({ status: 400, message: 'Internal Server Error' });
-=======
 
 /** @description Cette route permet d'obtenir un seul post avec son id /api/p/{postId}  */
 router.get('/:id', async (req, res) => {
@@ -81,7 +45,6 @@ router.get('/:id', async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status();
->>>>>>> 0103907c26d141ee92ec9710cac9ace4754bf9c7
   }
 });
 // /**
@@ -90,22 +53,6 @@ router.get('/:id', async (req, res) => {
 //  *
 //  */
 
-<<<<<<< HEAD
-router.post('/update', async (req, res) => {
-  try {
-    const p = req.body;
-    await userRepository.update(p);
-    return res
-      .status(201)
-      .end();
-  } catch (err) {
-    console.error(err);
-    return res
-      .status(400)
-      .json({ status: 400, message: 'Internal Server Error' });
-  }
-});
-=======
 // // ROUTE POUR GET LES POSTS
 // router.get("/", async (req, res) => {
 //   try {
@@ -165,6 +112,5 @@ router.post('/update', async (req, res) => {
 //     res.status(500).json({ status: 500, message: 'Internal Server Error' });
 //   }
 // });
->>>>>>> 0103907c26d141ee92ec9710cac9ace4754bf9c7
 
 module.exports = router;
