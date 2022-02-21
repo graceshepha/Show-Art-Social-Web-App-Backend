ee039a10c217df5177bca56cb3f5c00a06a84920
xmaiying <58544027+xmaiying@users.noreply.github.com>
2022-02-21 13:49:42 -0500

    tentativeAvecId

--- routes/api/posts/index.js
+++ routes/api/posts/index.js
@@ -40,79 +40,17 @@ router.get('/', async (req, res) => {
   }
 });
 
+// ESSAYER DE CONTINUER TRUC DE GRACE
+
 /** @description Cette route permet d'obtenir un seul post avec son id /api/p/{postId}  */
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
+    const post = await postRepository.getOne(id);
   } catch (err) {
     console.error(err);
     res.status();
   }
 });
-// /**
-//  *
-//  * @author My-Anh Chau
-//  *
-//  */
-
-// // ROUTE POUR GET LES POSTS
-// router.get("/", async (req, res) => {
-//   try {
-//     const items = await getToutPosts(req.userId);
-//     res.status(200).json(items);
-//   } catch {
-//     res.status(500).json({ status: 500, message: "Internal Server Error" });
-//   }
-// });
-
-// // ROUTE POUR GET UN POST AVEC ID
-// router.get("/:idPost", async (req, res) => {
-//   const { idPost, idTag } = req.query;
-//   try {
-//     if (!idPost || !idTag) {
-//       res.status(400).json({ status: 400, message: "Bad Request" });
-//     } else {
-//       await addPost({ idUtilisateur: req.userId, idPost, idTag });
-//       res.status(200).end();
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ status: 500, message: "Internal Server Error" });
-//   }
-// });
-
-// // ROUTE POUR AJOUTER UN POST
-// router.post("/", async (req, res) => {
-//   const { idPost, idTag } = req.query;
-//   try {
-//     if (!idPost || !idTag) {
-//       res.status(400).json({ status: 400, message: 'Bad Request' });
-//     } else {
-//       await addPost({ idUtilisateur: req.userId, idPost, idTag });
-//       res.status(200).end();
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ status: 500, message: 'Internal Server Error' });
-//   }
-// });
-
-// // ROUTE POUR ENLEVER UN POST
-// router.delete("/p", async (req, res) => {
-//   const { idPost, idTag } = req.query;
-//   try {
-//     if (!idPost || !idTag) {
-//       res.status(400).json({ status: 400, message: 'Bad Request' });
-//     } else {
-//       await removePost({
-//         idUtilisateur: req.userId,
-//         idPost,
-//       });
-//       res.status(200).end();
-//     }
-//   } catch {
-//     res.status(500).json({ status: 500, message: 'Internal Server Error' });
-//   }
-// });
 
 module.exports = router;
